e8018062771daf24cdd0c7bb8717eefc5299f292
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2025-06-04 19:22:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Daniel Werner</h1>
+//         <p>Software Engineer</p>
+//       </header>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Daniel Werner</h1>
-        <p>Software Engineer</p>
-      </header>
+    <div className="text-3xl font-bold underline">
+      Hello Tailwind!
     </div>
   );
 }
+
 
 export default App;
